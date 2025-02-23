6857a8d89aa74b23afcace412151d5990559aa5d
le1nism <cristian.capiferri@gmail.com>
2025-02-23 23:49:16 +0100

    Fixed texture orientation issue

--- textures.py
+++ textures.py
@@ -8,6 +8,7 @@ class Textures:
 
 		# Load texture
 		self.texture_0 = self.load('frame.png')
+		# self.texture_0 = self.load('test.png')
 
 		# Assign texture unit
 		self.texture_0.use(location = 0)
